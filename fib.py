861f7f5b85ed509e5de67b2a48214b10b49a89f9
Eric Schles <ericschles@gmail.com>
2016-01-02 22:51:09 -0500

    adding setup.py file

---
+++ fib.py
@@ -0,0 +1,26 @@
+from time import time
+
+def fib(x):
+    if x == 0:
+        return 1
+    if x == 1:
+        return 1
+    else:
+        return fib(x-1) + fib(x-2)
+
+def main():
+    start = time()
+    fib(3)
+    end = time() - start
+    print "fib(3) took",end,"seconds"
+    start = time()
+    fib(15)
+    end = time() - start
+    print "fib(15) took",end,"seconds"
+    start = time()
+    fib(50)
+    end = time() - start
+    print "fib(200) took",end,"seconds"
+    
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = "cplr",
+    version="0.1",
+    description="a simple wrapper around cython",
+    author="Eric Schles",
+    packages=["cplr"]
+)
